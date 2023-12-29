f637d563527b37fc626bb1664724dba7e5e9bb7d
Edwin Saul <edwinsaulpm@gmail.com>
2023-12-29 12:17:56 -0500

    cambios en bulma mailto

---
+++ experimentos_js/03_mailto/send-mail.js
@@ -0,0 +1,14 @@
+// writed by Edwin Saul
+
+function send_mail() {
+  var name_var ="namee"
+  var email_var="email@ffff.dd"
+  var content_email_var="hohohohohoh"
+
+  console.log(name_var);
+}
+
+
+
+
+
